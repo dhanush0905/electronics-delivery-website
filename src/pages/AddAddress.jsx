fb3e73ea8b20d70e7b4ee7d9956f86cfcb8f9761
Dhanush Peram <dhanushperam1@gmail.com>
2025-05-18 23:50:45 +0530

    Updated

--- src/pages/AddAddress.jsx
+++ src/pages/AddAddress.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
+const inputField= (()=>(
+  <input type="text"/>
+))
 const AddAddress = () => {
+
+  const onSubmitHandler= async(e)=>{
+    e.preventDefault();
+  }
   return (
-    <div>
-      
+    <div className='mt-16 pb-16'>
+      <p className='text-2xl md:text-3xl text-gray-500'> Add Shipping<span className='font-semibold text-primary'>Address</span></p>
+      <div className='flex flex-col-reverse md:flex-row justify-between mt-10'>
+        <div className='flex-1 max-w-md'>
+          <form onSubmit={onSubmitHandler} className='space-y-3 mt-6 text-sm'></form>
+        </div>
+        <img src={assets.add_address_iamge} alt='Add Address'/>
+
+      </div>
     </div>
   )
 }
