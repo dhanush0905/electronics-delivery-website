473f386bd9c53df9747ec8ef29b1983c49cfc8f3
Dhanush Peram <dhanushperam1@gmail.com>
2025-05-19 23:58:42 +0530

    Updated

--- src/pages/AddAddress.jsx
+++ src/pages/AddAddress.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets } from '../assets/assets'
 
-const inputField= (()=>(
-  <input type="text"/>
-))
+const inputField= ({type, placeholder,name, handleChange, address})=>(
+  <input className='w-full px-2 py-2.5 border border-gray-500/30 rounded outline-none text-gray-500 focus:border-primary transition'
+  type={type}
+  placeholder={placeholder}
+  onChange={handleChange}
+  name={name}
+  value={address[name]}
+  required
+  />
+)
 const AddAddress = () => {
 
+  const[address, setAddress]= useState({
+    firstName:'',
+    lastName:'',
+    email:'',
+    street:'',
+    city:'',
+    state:'',
+    zipcode:'',
+    country:'',
+    phone:'',
+  })
   const onSubmitHandler= async(e)=>{
     e.preventDefault();
   }
@@ -14,7 +32,12 @@ const AddAddress = () => {
       <p className='text-2xl md:text-3xl text-gray-500'> Add Shipping<span className='font-semibold text-primary'>Address</span></p>
       <div className='flex flex-col-reverse md:flex-row justify-between mt-10'>
         <div className='flex-1 max-w-md'>
-          <form onSubmit={onSubmitHandler} className='space-y-3 mt-6 text-sm'></form>
+          <form onSubmit={onSubmitHandler} className='space-y-3 mt-6 text-sm'>
+            <div>
+              <inputField handleChange={handleChange} address={address} name='firstName' type="text" placeholder="First Name"/>
+              <inputField handleChange={handleChange} address={address} name='lastName' type="text" placeholder="Last Name"/>
+            </div>
+          </form>
         </div>
         <img src={assets.add_address_iamge} alt='Add Address'/>
 
