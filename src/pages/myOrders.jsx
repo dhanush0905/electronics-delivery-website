da14cf193c39a57e5c1d5e005b5be720dcee3ded
Dhanush Peram <dhanushperam1@gmail.com>
2025-05-23 23:54:35 +0530

    Updated

--- src/pages/myOrders.jsx
+++ src/pages/myOrders.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAppContext } from '../context/AppContext'
+import { dummyOrders } from '../assets/assets'
 
 const MyOrders = () => {
+  const[myOrders, setMyOrders]= useState([])
+  const {currency}= useAppContext()
+
+  const fetchMyOrders= async()=>{
+    setMyOrders(dummyOrders)
+  }
+
+  useEffect(()=>{
+    fetchMyOrders()
+  },[])
   return (
-    <div>
-      <p>My Orders</p>
+    <div className='mt-=16 pb-16'>
+      <div className='flex flex-col itmes-end w-max mb-8'>
+        <p className='text-2xl font-medium uppercase'>My Orders</p>
+        <div className='w-16 h-0.5 bg-primary rounded-full'>
+        </div>
+        {myOrders.map((order,index)=>(
+          <div>
+            <p>
+              <span>OrderId:{order._id}</span>
+              <span>Payment:{order.paymentType}</span>
+              <span>Total Amount:{currency}{order.amount}</span>
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
