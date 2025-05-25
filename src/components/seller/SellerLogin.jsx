ad7c1a2eee2b9da61627585dda79ce726f682e10
Dhanush Peram <dhanushperam1@gmail.com>
2025-05-25 23:31:22 +0530

    Init

---
+++ src/components/seller/SellerLogin.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useAppContext } from '../../context/AppContext'
+
+const SellerLogin = () => {
+    const {isSeller, setIsSeller,navigate}= useAppContext()
+    const [email, setEmail]= useState("");
+    const[password, setPassword]= useState("");
+
+    const onSubmitHandler= async(event)=>{
+        event.preventDefault();
+        setIsSeller(true)
+    }
+
+    useEffect(()=>{
+        if(isSeller){
+            navigate("/seller")
+        }
+    },[isSeller])
+
+  return !isSeller &&(
+    <form onSubmit={onSubmitHandler} className='min-h-screen flex items-center text-sm text-gray-600'>
+        <div className='flex flex-col gap-5 m-auto items-start p-8 py-12 min-w-80 sm:min-w-88 rounded-lg shadow-xl border border-gray-200'>
+            <p className='text-2xl font-medium m-auto'><span className='text-primary'>Seller</span>Login</p>
+        </div>
+    </form>
+  )
+}
+
+export default SellerLogin
