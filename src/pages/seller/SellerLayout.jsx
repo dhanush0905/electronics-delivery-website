6adeaae62cae98f9adf3387492ab5295a6cddd7a
Dhanush Peram <dhanushperam1@gmail.com>
2025-05-30 23:23:14 +0530

    Updated

--- src/pages/seller/SellerLayout.jsx
+++ src/pages/seller/SellerLayout.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { assets } from "../../assets/assets";
 import {useAppContext} from "../../context/AppContext"
 const SellerLayout = () => {
@@ -14,10 +15,9 @@ const SellerLayout = () => {
 
     return (
         <>
-            <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white transition-all duration-300">
-                <a href="/">
-                    <img className="h-9" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/dummyLogo/dummyLogoColored.svg" alt="dummyLogoColored" />
-                </a>
+            <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white">
+                <Link to="/">
+                </Link>
                 <div className="flex items-center gap-5 text-gray-500">
                     <p>Hi! Admin</p>
                     <button className='border rounded-full text-sm px-4 py-1'>Logout</button>
